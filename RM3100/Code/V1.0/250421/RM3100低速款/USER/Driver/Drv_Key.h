f4458746e7136c3dad02a9b7cc6e4b14fbc4d1ca
Kangaroo <413905516@qq.com>
2025-04-23 09:17:20 +0800

    加入了低速款，然后发现定时有问题，23.59分启动变成24.00，然后改好了

---
+++ RM3100/Code/V1.0/250421/RM3100低速款/USER/Driver/Drv_Key.h
@@ -0,0 +1,22 @@
+#ifndef __DRV_KEY_H__
+#define	__DRV_KEY_H__
+
+#include "include.h"
+
+/**
+********宏定义************/
+#define KEY1 HAL_GPIO_ReadPin(KEY1_GPIO_Port,KEY1_Pin)
+#define KEY2 HAL_GPIO_ReadPin(KEY2_GPIO_Port,KEY2_Pin)
+#define KEY3 HAL_GPIO_ReadPin(KEY3_GPIO_Port,KEY3_Pin)
+#define KEY4 HAL_GPIO_ReadPin(KEY4_GPIO_Port,KEY4_Pin)
+
+/**********结构体************/
+/**********全局变量声明******/
+extern uint8_t Key_Status;//按键按下标志
+
+/**********局部变量声明******/
+/**********全局函数**********/
+void Key_Scan(float dT);//矩阵按键扫描
+void Check_Press(float dT);//检测按键按下状态
+
+#endif
