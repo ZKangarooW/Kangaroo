c49489579fb2e1a7689632f88a652ec24cef355a
kangaroo <413905516@qq.com>
2025-06-05 01:18:29 +0000

    程序优化，RGB功能加入，然后开关上电不会直接亮，关了再开单片机是直接重启，不是改变标志位

---
+++ HS3500/Code/V1.0/250524/HS3500/Core/Inc/main.h
@@ -0,0 +1,117 @@
+/* USER CODE BEGIN Header */
+/**
+  ******************************************************************************
+  * @file           : main.h
+  * @brief          : Header for main.c file.
+  *                   This file contains the common defines of the application.
+  ******************************************************************************
+  * @attention
+  *
+  * Copyright (c) 2025 STMicroelectronics.
+  * All rights reserved.
+  *
+  * This software is licensed under terms that can be found in the LICENSE file
+  * in the root directory of this software component.
+  * If no LICENSE file comes with this software, it is provided AS-IS.
+  *
+  ******************************************************************************
+  */
+/* USER CODE END Header */
+
+/* Define to prevent recursive inclusion -------------------------------------*/
+#ifndef __MAIN_H
+#define __MAIN_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/* Includes ------------------------------------------------------------------*/
+#include "stm32f0xx_hal.h"
+
+/* Private includes ----------------------------------------------------------*/
+/* USER CODE BEGIN Includes */
+#include "include.h"
+/* USER CODE END Includes */
+
+/* Exported types ------------------------------------------------------------*/
+/* USER CODE BEGIN ET */
+
+/* USER CODE END ET */
+
+/* Exported constants --------------------------------------------------------*/
+/* USER CODE BEGIN EC */
+
+/* USER CODE END EC */
+
+/* Exported macro ------------------------------------------------------------*/
+/* USER CODE BEGIN EM */
+
+/* USER CODE END EM */
+
+/* Exported functions prototypes ---------------------------------------------*/
+void Error_Handler(void);
+
+/* USER CODE BEGIN EFP */
+
+/* USER CODE END EFP */
+
+/* Private defines -----------------------------------------------------------*/
+#define KEY5_Pin GPIO_PIN_13
+#define KEY5_GPIO_Port GPIOC
+#define Beep_Pin GPIO_PIN_14
+#define Beep_GPIO_Port GPIOC
+#define AD_T1_Pin GPIO_PIN_1
+#define AD_T1_GPIO_Port GPIOA
+#define AD_T2_Pin GPIO_PIN_2
+#define AD_T2_GPIO_Port GPIOA
+#define KEY2_Pin GPIO_PIN_3
+#define KEY2_GPIO_Port GPIOA
+#define PWM_Pin GPIO_PIN_4
+#define PWM_GPIO_Port GPIOA
+#define REF_2_5V_Pin GPIO_PIN_5
+#define REF_2_5V_GPIO_Port GPIOA
+#define LED_ADJ_Pin GPIO_PIN_6
+#define LED_ADJ_GPIO_Port GPIOA
+#define HEAT_Pin GPIO_PIN_7
+#define HEAT_GPIO_Port GPIOA
+#define CW_Pin GPIO_PIN_0
+#define CW_GPIO_Port GPIOB
+#define RGB_Pin GPIO_PIN_1
+#define RGB_GPIO_Port GPIOB
+#define Key_POR_Pin GPIO_PIN_2
+#define Key_POR_GPIO_Port GPIOB
+#define EC2A_Pin GPIO_PIN_10
+#define EC2A_GPIO_Port GPIOB
+#define EC2A_EXTI_IRQn EXTI4_15_IRQn
+#define EC2B_Pin GPIO_PIN_11
+#define EC2B_GPIO_Port GPIOB
+#define KEY1_Pin GPIO_PIN_12
+#define KEY1_GPIO_Port GPIOB
+#define EC1B_Pin GPIO_PIN_13
+#define EC1B_GPIO_Port GPIOB
+#define EC1A_Pin GPIO_PIN_14
+#define EC1A_GPIO_Port GPIOB
+#define EC1A_EXTI_IRQn EXTI4_15_IRQn
+#define FG_Pin GPIO_PIN_8
+#define FG_GPIO_Port GPIOA
+#define Ht162x_DAT_Pin GPIO_PIN_4
+#define Ht162x_DAT_GPIO_Port GPIOB
+#define Ht162x_WR_Pin GPIO_PIN_5
+#define Ht162x_WR_GPIO_Port GPIOB
+#define Ht162x_CS_Pin GPIO_PIN_7
+#define Ht162x_CS_GPIO_Port GPIOB
+#define KEY3_Pin GPIO_PIN_8
+#define KEY3_GPIO_Port GPIOB
+#define KEY4_Pin GPIO_PIN_9
+#define KEY4_GPIO_Port GPIOB
+
+/* USER CODE BEGIN Private defines */
+
+/* USER CODE END Private defines */
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* __MAIN_H */
---
+++ HS5100/Code/V1.0/250526/HS5100/USER/Driver/Drv_PT1000.h
@@ -0,0 +1,26 @@
+#ifndef __DRV_PT1000_H__
+#define __DRV_PT1000_H__
+
+#include "include.h"
+
+/**********宏定义************/
+#define N           100//互动滤波采集100次
+#define ALPHA       0.1f//平滑因子，调整响应速度
+
+#define Vref		1.2f//内部参考电压
+#define OP_Value	6.8f//放大系数
+#define R_3K        3.0f//分压电阻3k
+#define R_910R      0.91f//分压电阻910Ω
+
+#define AD_LEN      4//DMA获取长度
+
+/**********全局变量**********/
+extern float PT_VALUE_1_TEMP;//外部探头阻值
+extern float PT_VALUE_2_TEMP;//台面探头阻值
+
+/**********全局函数**********/
+void ADCDMA_Init(void);//ADC和DMA的初始化
+void Read_Temp(float dT);//读取温度-10ms
+float CelsiusToFahrenheit(uint8_t type,int16_t celsius);//摄氏度转华氏度
+
+#endif
